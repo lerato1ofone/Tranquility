38feec2f9be5852b2cf37618a0c413aaf8c14f81
Lerato1OFONE <letsepe100@gmail.com>
2021-08-30 02:45:57 +0200

    Added icons (svg components)

---
+++ frontend/components/icons/BriefcaseIcon.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Briefcase() {
+    return (
+        <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M11.6666 4.08333H2.33329C1.68896 4.08333 1.16663 4.60566 1.16663 5.24999V11.0833C1.16663 11.7277 1.68896 12.25 2.33329 12.25H11.6666C12.311 12.25 12.8333 11.7277 12.8333 11.0833V5.24999C12.8333 4.60566 12.311 4.08333 11.6666 4.08333Z" stroke="white" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M9.33329 12.25V2.91667C9.33329 2.60725 9.21038 2.3105 8.99158 2.09171C8.77279 1.87292 8.47605 1.75 8.16663 1.75H5.83329C5.52387 1.75 5.22713 1.87292 5.00833 2.09171C4.78954 2.3105 4.66663 2.60725 4.66663 2.91667V12.25" stroke="white" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+         
+    )
+}
+
+export default Briefcase
---
+++ frontend/components/icons/InstagramIcon.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function InstagramIcon() {
+    return (
+        <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12.75 1.5H5.25C3.17893 1.5 1.5 3.17893 1.5 5.25V12.75C1.5 14.8211 3.17893 16.5 5.25 16.5H12.75C14.8211 16.5 16.5 14.8211 16.5 12.75V5.25C16.5 3.17893 14.8211 1.5 12.75 1.5Z" stroke="white" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M11.9998 8.5275C12.0923 9.15168 11.9857 9.78916 11.6951 10.3493C11.4045 10.9094 10.9446 11.3636 10.381 11.6473C9.81736 11.9309 9.17862 12.0297 8.55562 11.9294C7.93262 11.8292 7.3571 11.5351 6.91091 11.0889C6.46472 10.6427 6.17058 10.0672 6.07033 9.44416C5.97008 8.82116 6.06883 8.18242 6.35253 7.61878C6.63622 7.05514 7.09042 6.5953 7.65052 6.30468C8.21062 6.01405 8.8481 5.90744 9.47228 6C10.109 6.09441 10.6984 6.39109 11.1536 6.84623C11.6087 7.30136 11.9054 7.89081 11.9998 8.5275Z" stroke="white" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M13.125 4.875H13.1325" stroke="white" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+    )
+}
+
+export default InstagramIcon
