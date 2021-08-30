9982473fa9006f37fab71146287bb089229e8881
Lerato1OFONE <letsepe100@gmail.com>
2021-08-30 02:46:32 +0200

    Added header and global styles

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.scss'
 import Link from "next/link"
+import styles from '../styles/Home.module.scss'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
@@ -8,14 +9,18 @@ export default function Home() {
       <Head>
         <title>Tranquility Software</title>
         <meta name="description" content="Tranquility Software Pretoria" />
-        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Header />
+      
       <main>
+        {/* <Landing /> */}
       </main>
 
       <footer>
       </footer>
+  
     </div>
   )
 }
