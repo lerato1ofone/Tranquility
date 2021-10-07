9a09550b29b3fbcce8e1eefbc673c71841343ed8
Lerato Letsepe <letsepe100@gmail.com>
2021-10-08 00:17:41 +0200

    Added a content wrapper for landing page content

--- frontend/components/Landing.js
+++ frontend/components/Landing.js
@@ -2,11 +2,15 @@ import React from 'react'
 import styles from '../styles/Landing.module.scss'
 import LandingSlider from './LandingSlider'
 import sliderData from '../data/sliderData.json';
+import WhatWeDo from './WhatWeDo';
 
 function Landing() {
     return (
         <div>
           <LandingSlider slides={sliderData}/>
+          <div className={styles['content-wrapper']}>
+            <WhatWeDo />
+          </div>
         </div>
     )
 }
