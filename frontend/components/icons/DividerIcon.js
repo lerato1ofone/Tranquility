1725e6f98b6d86e1f740ee9f86d234cfdc053c84
Lerato Letsepe <letsepe100@gmail.com>
2021-10-08 00:20:01 +0200

    Added a divider icon

---
+++ frontend/components/icons/DividerIcon.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DividerIcon() {
+    return (
+        <svg width="924" height="1" viewBox="0 0 924 1" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="924" height="1" rx="0.5" fill="#303030"/>
+        </svg>
+    )
+}
+
+export default DividerIcon
