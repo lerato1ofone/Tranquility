7b3c6edc05f243557de364474f873794b8fd4ab7
Lerato Letsepe <letsepe100@gmail.com>
2021-10-25 18:59:09 +0200

    Added what we do screen

--- frontend/components/WhatWeDo.js
+++ frontend/components/WhatWeDo.js
@@ -1,12 +1,25 @@
 import React from 'react'
-import { TEXT_TITLE } from '../constants/titleConstants'
+import { TEXT_SUBTITLE, TEXT_TITLE } from '../constants/titleConstants'
 import styles from '../styles/WhatWeDo.module.scss'
 import Text from './Text'
+import DividerIcon from './icons/DividerIcon'
+import ServiceCard from './ServiceCard'
 
 function WhatWeDo() {
     return (
         <div className={styles.container}>
-              <Text text="What we do" type={TEXT_TITLE} className={styles['text-color']} />
+            <div className={styles.heading}>
+              <Text text="What we do" type={TEXT_SUBTITLE} className={styles['text-color']} />
+             <div className={styles.end}>
+                <DividerIcon />
+             </div>
+            </div>
+            <div className={styles.content}>
+                <ServiceCard icon='design' title='UI/UX Design' subtitles={['Design thinking', 'User-centered design', 'Interactive design', 'Agile']} />
+                <ServiceCard icon='branding' title='Branding' subtitles={['Critical thinking', 'Concept development', 'Story telling', 'Result orientation']} />
+                <ServiceCard icon='dev' title='Software development' subtitles={['Full Stack Web Development', 'Mobile App development', 'Interactive design', 'Agile']} />
+                <ServiceCard icon='marketing' title='Marketing' subtitles={['Analytical', 'Critical thinking', 'Research', 'Result orientated']} />
+            </div>
         </div>
     )
 }
--- frontend/components/icons/DividerIcon.js
+++ frontend/components/icons/DividerIcon.js
@@ -5,6 +5,7 @@ function DividerIcon() {
         <svg width="924" height="1" viewBox="0 0 924 1" fill="none" xmlns="http://www.w3.org/2000/svg">
             <rect width="924" height="1" rx="0.5" fill="#303030"/>
         </svg>
+
     )
 }
 
